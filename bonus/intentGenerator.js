a1b1687a81a04e0139a2830ffb5f2f6952bba6d9
knowlsie <tomobknowles@gmail.com>
2017-04-05 15:54:24 +0100

    Improve intent generator

--- bonus/intentGenerator.js
+++ bonus/intentGenerator.js
@@ -4,13 +4,13 @@ const intentName = "";
  * intentGenerator - Combines provided arrays of words to create arrays.
  * If an array is skippable, simply add an empty string to it.
  *
- * @param  {...string[]} args arrays of string, in order to be put.
+ * @param  {...string[]} args arrays of strings, in the order of writing.
  * @return {undefined}
  */
 function intentGenerator(...args) {
   (function permutateConsole(result, rest) {
     if (rest.length === 0) {
-      console.log(intentName + result);
+      console.log(result);
       return;
     }
     rest[0].forEach((next) => {
@@ -20,7 +20,7 @@ function intentGenerator(...args) {
         permutateConsole(result + " " + next, rest.slice(1));
       }
     });
-  }("", args));
+  }(intentName, args));
 }
 
 intentGenerator(["Book", "Find", "Get", "Look for", "Make"], ["me", "us", ""], ["a", "the", ""], ["room.", "meeting room.", "meeting."]);
